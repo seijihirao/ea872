b64bec09284cc016a3f14e72a466527d20e45ddd
Rodrigo Seiji Piubeli Hirao <rodrigo@kissolutions.tech>
2021-09-30 14:40:59 -0300

    Fix: texture.

--- include/models/texture.h
+++ include/models/texture.h
@@ -8,12 +8,12 @@ using namespace std;
 
 class Texture {
     protected:
-        SDL_Rect target;
+        SDL_Rect* target;
         SDL_Texture* texture;
 
     public:
         SDL_Rect* getTarget();
-        void setTarget(SDL_Rect target);
+        void setTarget(SDL_Rect* target);
         SDL_Texture* getTexture();
         void setTexture(SDL_Texture* texture);
 
